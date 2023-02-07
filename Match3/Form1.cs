7af254de2cebf998a07eb3c6c3257629365fe47d
Joshua Price <32619882+joshuawprice@users.noreply.github.com>
2023-02-07 17:02:24 +0000

    Add grid of buttons
    Adds some code to be modified later when creating the rest of the game.

--- Match3/Form1.cs
+++ Match3/Form1.cs
@@ -1,10 +1,27 @@
-namespace Match3
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace Match3 {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
+            Random rng = new Random();
+
+            // Setup grid of buttons
+            Button[,] Buttons = new Button[9, 9];
+
+            for (int i = 0; i < Buttons.GetLength(0); i++) {
+                for (int j = 0; j < Buttons.GetLength(1); j++) {
+                    Buttons[i, j] = new Button();
+                    Buttons[i, j].SetBounds(60 * i, 60 * j, 60, 60);
+                    Buttons[i, j].BackColor = Color.FromArgb(rng.Next(0, 256), rng.Next(0, 256), rng.Next(0, 256));
+                    Controls.Add(Buttons[i, j]);
+                }
+            }
+
+
+            // Add link to the event handler to use when clicked
+            //btn.Click += new EventHandler(btnEvent_Click);
+        }
+        void btnEvent_Click(object sender, EventArgs e) {
+            //btn.BackColor = Color.Red;
         }
     }
 }
